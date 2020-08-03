b5206e685b5be8c057bc2566e597e21ca25668f3
Viacheslav <fedorov_slava@mail.ru>
2020-08-03 22:50:50 +0300

    Add test button

--- test_items.py
+++ test_items.py
@@ -1,13 +1,12 @@
-def test_alien(browser):
+
+from selenium.webdriver.support.ui import Select
+
+def test_find_button(browser):
     link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
     browser.get(link)
     browser.implicitly_wait(7)
-    answer = math.log(int(time.time()))
-    y = str(answer)
-    input1 = browser.find_element_by_css_selector('[placeholder="Напишите ваш ответ здесь..."]')
-    input1.send_keys(y)
-    button = browser.find_element_by_class_name("submit-submission")
-    button.click()
-    correct_name = browser.find_element_by_class_name("smart-hints__hint")
-    correct = correct_name.text
-    assert "Correct!" == correct, f'Не совпадает фидбек!, Выводит: {correct}.'
+
+    select = Select(browser.find_element_by_tag_name("select"))
+    select.select_by_value("es")
+    button = browser.find_element_by_class_name("btn-add-to-basket")
+    assert button == True,  "Караул, кнопку украли!"
