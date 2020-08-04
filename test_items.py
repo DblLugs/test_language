f0f71898866a7a93a99e8b05ef5907616402bd1d
DblLugs <64919196+DblLugs@users.noreply.github.com>
2020-08-04 18:16:18 +0300

    Update test_items.py

--- test_items.py
+++ test_items.py
@@ -4,6 +4,6 @@ import time
 def test_find_button_add_to_basket(browser):
     link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
     browser.get(link)
-    time.sleep(10)
+    time.sleep(30)
     button = browser.find_elements_by_class_name("btn-add-to-basket")
     assert len(button) == 1,  "OMG! Button was stolen!"
