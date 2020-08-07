0284367223fdc4f651083c0e4371670c11010fa7
DblLugs <64919196+DblLugs@users.noreply.github.com>
2020-08-07 14:34:39 +0300

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -6,8 +6,6 @@ from selenium.webdriver.chrome.options import Options
 def pytest_addoption(parser):
     parser.addoption('--browser_name', action='store', default='chrome',
                      help="Choose browser: chrome or firefox")
-
-def pytest_addoption(parser):
     parser.addoption('--language', action='store', default='ru',
                      help="Choose language: ru, es, fr... ")
 
